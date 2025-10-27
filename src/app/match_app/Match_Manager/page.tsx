10474758a0e57114fc0ff9f56decd4edb5897155
narutounzumaki26-create <narutounzumaki26@gmail.com>
2025-10-27 20:29:38 +0100

    Update page.tsx

--- src/app/match_app/Match_Manager/page.tsx
+++ src/app/match_app/Match_Manager/page.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from 'react'
 
-import { supabase } from '@/lib/supabase'
+import { supabase } from "@/lib/supabaseClient";
 
 // Types defined directly in the component file
 interface Match {
