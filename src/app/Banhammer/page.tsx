c8f42169849a2a8aa914c15bf8118d876fcf4672
narutounzumaki26-create <narutounzumaki26@gmail.com>
2025-11-03 15:03:54 +0100

    Update page.tsx

--- src/app/Banhammer/page.tsx
+++ src/app/Banhammer/page.tsx
@@ -4,7 +4,7 @@ export default function SimpleBackgroundPage() {
     <div 
       className="min-h-screen flex items-center justify-center"
       style={{
-        backgroundImage: "url('https://i.ytimg.com/vi/dQxgV4oD4J4/maxresdefault.jpg')", // Replace with your image path
+        backgroundImage: "url('https://media.tenor.com/znx_AntI870AAAAM/gorilla-middle-finger.gif')", // Replace with your image path
         backgroundSize: "cover",
         backgroundPosition: "center"
       }}
@@ -15,7 +15,7 @@ export default function SimpleBackgroundPage() {
       {/* Centered text */}
       <div className="relative z-10 text-center text-white space-y-4 p-8">
         <h1 className="text-4xl md:text-6xl font-bold">T&apos;as été ban</h1>
-        <p className="text-xl md:text-2xl opacity-90">Celui qui nous trompe n&apos;est pas des notres. Mais si tu t&apos;es egaré ton chemin alors viens nous mp :3 </p>
+        <p className="text-xl md:text-2xl opacity-90">Celui qui nous trompe n&apos;est pas des notres. Mais si tu as egaré ton chemin alors viens nous mp :3 </p>
         <p className="text-xl md:text-2xl opacity-90">Cordialement X-Rank Team </p>
       </div>
     </div>
