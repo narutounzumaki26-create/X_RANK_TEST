71ab22b3f204caa517a47535a4bc79997516f1df
narutounzumaki26-create <narutounzumaki26@gmail.com>
2025-11-05 18:52:41 +0100

    Update supabaseClient.ts

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,7 +1,7 @@
 // lib/supabaseClient.ts
 import { createClient } from '@supabase/supabase-js'
 
-export const supabasepwd = createClient(
+export const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!)
 
